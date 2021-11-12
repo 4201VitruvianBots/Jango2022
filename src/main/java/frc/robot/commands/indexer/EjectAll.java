deb2f452676da43eb631df88fc75b319cf96bbfb
Benjamin Jonson <ben332004@gmail.com>
2021-11-12 09:52:27 -0800

    Copied indexer and intake commands, not cleaned upyet

---
+++ src/main/java/frc/robot/commands/indexer/EjectAll.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.indexer;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
+
+/**
+ * TODO: Add description.
+ */
+public class EjectAll extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final Indexer m_indexer;
+    private final Intake m_intake;
+
+    /**
+     * Creates a new EjectAll.
+     */
+    public EjectAll(Indexer indexer, Intake intake) {
+        m_indexer = indexer;
+        m_intake = intake;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(indexer);
+        addRequirements(intake);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_indexer.setIndexerOutput(- 0.6);
+        m_indexer.setKickerOutput(- 0.5);
+        m_intake.setIntakePercentOutput(- 0.5);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(final boolean interrupted) {
+        m_indexer.setKickerOutput(0);
+        m_indexer.setIndexerOutput(0);
+        m_intake.setIntakePercentOutput(0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/main/java/frc/robot/commands/indexer/FeedAll.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.indexer;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Indexer;
+
+/**
+ * TODO: Add description
+ */
+public class FeedAll extends CommandBase {
+    private final Indexer m_indexer;
+    double m_setpoint;
+    private double startTime;
+    
+    /**
+     * Creates a new FeedAll.
+     *
+     * @param indexer The subsystem used by this command.
+     */
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    public FeedAll(Indexer indexer) {
+        m_indexer = indexer;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(indexer);
+
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_indexer.setIndexerOutput(0.6);
+        m_indexer.setKickerOutput(0.5);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(final boolean interrupted) {
+        m_indexer.setKickerOutput(0);
+        m_indexer.setIndexerOutput(0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        double time = Timer.getFPGATimestamp();
+        if(m_indexer.getIndexerTopSensor()) {
+            time = Timer.getFPGATimestamp();
+        }
+        return time >= 2;
+    }
+}
